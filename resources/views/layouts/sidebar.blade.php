3ae787bfb70e4df76eecc293dd6058ccfe8e9744
DESKTOP-OVUED56\SBLP10 <kamrul.singularity@gmail.com>
2023-11-10 10:47:58 +0600

    create master layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar-wrapper">
+    <div class="sidebar sidebar-collapse" id="sidebar">
+       <div class="sidebar__menu-group">
+          <ul class="sidebar_nav">
+             <li class="has-child open">
+                <a href="#" class="active">
+                   <span class="nav-icon uil uil-create-dashboard"></span>
+                   <span class="menu-text">Dashboard</span>
+                   {{-- <span class="toggle-icon"></span> --}}
+                </a>
+             </li>
+             
+             <li class="has-child">
+                <a href="#" class="">
+                   <span class="nav-icon uil uil-bag"></span>
+                   <span class="menu-text text-initial">Task</span>
+                   {{-- <span class="toggle-icon"></span> --}}
+                </a>
+             </li>
+            
+          </ul>
+       </div>
+    </div>
+ </div>
